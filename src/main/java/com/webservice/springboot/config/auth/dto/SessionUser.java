f3537ee489e86b8511eef3f25267f8aead084e92
seonyeong10 <suny951013@naver.com>
2023-03-14 22:15:42 +0900

    구글 로그인, 로그아웃, 회원가입, 권한관리 기능 추가

---
+++ src/main/java/com/webservice/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,22 @@
+package com.webservice.springboot.config.auth.dto;
+
+import com.webservice.springboot.domain.user.User;
+import lombok.Getter;
+
+/**
+ * 세션에 저장하기 위한 직렬화된 dto
+ * 인증된 사용자 정보만 필요
+ */
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getEmail();
+    }
+}

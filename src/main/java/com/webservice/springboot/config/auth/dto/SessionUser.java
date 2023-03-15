dd8afb093fc955ee7a96d528b275640f6ed8e116
seonyeong10 <suny951013@naver.com>
2023-03-15 15:03:09 +0900

    session 저장소로 데이터베이스 사용 500 에러 발생 - SessionUser 에 Serializable 상속

--- src/main/java/com/webservice/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/webservice/springboot/config/auth/dto/SessionUser.java
@@ -3,13 +3,15 @@ package com.webservice.springboot.config.auth.dto;
 import com.webservice.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 /**
  * 세션에 저장하기 위한 직렬화된 dto
  * 인증된 사용자 정보만 필요
  */
 
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
